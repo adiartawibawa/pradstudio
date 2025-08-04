df71611167780f87c57e111490915c889e64c3d9
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2025-08-04 15:45:54 +0800

    Fix file paths in HTML and Vite configuration; add blog and post pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({  
-    plugins: [
-        tailwindcss(),
-    ],
-})
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        landing: resolve(__dirname, 'pages/index.html'),
+        blog: resolve(__dirname, 'pages/blog/index.html'),
+        // Add more entries for each page
+      },
+    },
+  },
+});
